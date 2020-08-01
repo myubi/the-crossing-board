3db06fc9faba725810d25e91296b692c0152df85
Tatiana Bernardo <tatianab@eu.square-enix.com>
2020-08-01 11:35:37 +0100

    Add podcasts json

--- scripts/generate-podcast-xml.js
+++ scripts/generate-podcast-xml.js
@@ -2,23 +2,16 @@ const fs = require('fs');
 
 const glob = require('glob');
 
-(async () => {
+const podcasts = require('../data/podcasts.json');
 
-  const podcasts = glob.sync('public/podcasts/**/*.mp3');
-  const podcastFileName = podcasts.map(file =>
-    file
-      .split('/')[2]
-      .replace(/ /g, '-')
-      .slice(0, -4)
-      .trim()
-  );
+(async () => {
   
   const podcastXML = `<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:rawvoice="http://www.rawvoice.com/rawvoiceRssModule/" version="2.0">
         <channel>
             <title>Fireside Chat</title>
             <link>https://www.thecrossingboard.com</link>
             <image>
-                <url>https://thecrossingboard.com/images/FridayFiresideChat.jpg</url>
+                <url>https://thecrossingboard.com/images/FridayFiresideChat.png</url>
                 <title>Fireside Chat</title>
                 <link>https://www.thecrossingboard.com</link>
             </image>
@@ -42,34 +35,34 @@ const glob = require('glob');
             <itunes:keywords>
                 animal crossing,video games, nintendo
             </itunes:keywords>
-            <itunes:image href="https://thecrossingboard.com/images/FridayFiresideChat.jpg"/>
+            <itunes:image href="https://thecrossingboard.com/images/FridayFiresideChat.png"/>
             <rawvoice:rating>TV-G</rawvoice:rating>
             <rawvoice:frequency>Weekly</rawvoice:frequency>
             <itunes:category text="Video Games"/>
             <pubDate>Fri, 31 Jul 2020 10:00:00 UTC</pubDate>
-            ${podcastFileName
-              .map((podcastItem, index) => {
+            ${podcasts.items
+              .map((podcast) => {
                 
                 return `
 
                         <item>
-                        <title>Fireside Chat ${index + 1}</title>
+                        <title>${podcast.name}</title>
                         <link>
-                            https://thecrossingboard.com/podcasts/${podcastItem}.mp3
+                            https://thecrossingboard.com/podcasts/${podcast.filename}
                         </link>
-                        <pubDate>Fri, 31 Jul 2020 10:00:00 UTC</pubDate>
+                        <pubDate>${podcast.timestamp}</pubDate>
                         <description>
-                            Friday Fireside Chats is a weekly podcast where we discuss anything and everything related to Animal Crossing! 
+                            ${podcast.description}
                         </description>
-                        <enclosure url="https://thecrossingboard.com/podcasts/${podcastItem}.mp3" length="36715125" type="audio/mpeg"/>
+                        <enclosure url="https://thecrossingboard.com/podcasts/${podcast.filename}" length="${podcast.filesize}" type="audio/mpeg"/>
                         <guid>
-                            https://thecrossingboard.com/podcasts/${podcastItem}.mp3
+                            https://thecrossingboard.com/podcasts/${podcast.filename}
                         </guid>
-                        <itunes:duration>19:07</itunes:duration>
+                        <itunes:duration>${podcast.duration}</itunes:duration>
                         <itunes:summary>
-                            Friday Fireside Chats is a weekly podcast where we discuss anything and everything related to Animal Crossing! 
+                            ${podcast.description}
                         </itunes:summary>
-                        <itunes:image href="https://thecrossingboard.com/images/FridayFiresideChat.jpg"/>
+                        <itunes:image href="https://thecrossingboard.com/images/FridayFiresideChat.png"/>
                         <itunes:keywords>
                             animal crossing, video games, nintendo
                         </itunes:keywords>
