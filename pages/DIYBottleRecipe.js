88ee87aa00d9155605c47603190133adf1edd3e8
Tatiana Bernardo <tatiana.mobe@gmail.com>
2021-01-06 15:53:11 +0000

    Add complimentary page to the magazine

---
+++ pages/DIYBottleRecipe.js
@@ -0,0 +1,51 @@
+import Layout from "../components/Layout";
+import matter from "gray-matter";
+import ReactMarkdown from "react-markdown";
+import Head from 'next/head';
+
+export default function Info({frontmatter, markdownBody}) {
+  const aboutFrontmatter = frontmatter;
+  const aboutBody = markdownBody;
+  
+  return (
+    <div>
+    <Head>
+      <title>The Crossing Board - DIY Bottle Recipe</title>
+    </Head>
+    <Layout>
+    <div className="section">
+    <div className="wrapper">
+      <ReactMarkdown source={aboutBody} escapeHtml={false} />
+    </div>
+
+    </div>
+    <style jsx>{`
+      .wrapper {
+        background-color: #fdf8e3;
+        margin: 20px auto;
+        border-radius: 10px;
+        padding: 20px;
+        color: #667756;
+        max-width: 60vw;
+      }
+      .wrapper :global(img) {
+        max-width: 50%;
+        margin: 0 auto;
+      }
+    `}</style>
+  </Layout>
+  </div>
+  );
+}
+
+export async function getStaticProps() {
+  const content = await import(`../data/DIYBottleRecipe.md`);
+  const data = matter(content.default);
+
+  return {
+    props: {
+      frontmatter: data.data,
+      markdownBody: data.content
+    },
+  }
+}
